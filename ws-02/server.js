cb2dd97f4c1fd25b3ca86bb1db7ce40e2a5d14a2
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-11-07 11:53:49 +0530

    create route

--- ws-02/server.js
+++ ws-02/server.js
@@ -4,6 +4,22 @@ require("dotenv").config();
 
 const port = process.env.port;
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "this is get route" });
+});
+
+app.post("/add", (req, res) => {
+  res.status(200).json({ message: "this is post route" });
+});
+
+app.put("/update", (req, res) => {
+  res.status(200).json({ message: "this is put route" });
+});
+
+app.delete("/delete", (req, res) => {
+  res.status(200).json({ message: "this is delete route" });
+});
+
 app.listen(port, () => {
   console.log(`server is listin on port ${port}`);
 });
