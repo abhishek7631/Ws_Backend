1a391bac83716e4ac9c58a02d9eaecc676aa7c68
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-11-11 13:15:31 +0530

    write home.jsx code

--- ws-04/frontend/src/Home.jsx
+++ ws-04/frontend/src/Home.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function Home() {
+  const [blog, setBlog] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5173/")
+      .then((res) => setBlog(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
-      <h1>This is home page</h1>
+      <h1>This is the Home Page</h1>
+      <div>
+        {blog.map((item, index) => (
+          <div
+            key={index}
+            style={{
+              marginBottom: "20px",
+              border: "1px solid #ddd",
+              padding: "10px",
+            }}
+          >
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+            {item.image && (
+              <img
+                src={item.image}
+                alt={item.title}
+                style={{ width: "100%", maxWidth: "400px", height: "auto" }}
+              />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
