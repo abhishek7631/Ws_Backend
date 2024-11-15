b28adfb634e7c5bcb1e7145336108c0924da59ab
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-11-15 13:15:12 +0530

    create login route

--- ws-07/server.js
+++ ws-07/server.js
@@ -39,6 +39,22 @@ app.post("/register", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "wrong credentials" });
+
+    const compare = await bcrypt.compare(password, user.password);
+
+    if (!compare) return res.status(400).json({ message: "wrong password" });
+
+    res.status(200).json({ message: "Login Successfull" });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+});
+
 app.listen(port, () => {
   console.log(`server is running at port ${port}`);
 });
