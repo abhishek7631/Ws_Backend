878bcbce887728563ee00cdebb38baddb2c71a34
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-11-19 12:09:04 +0530

    create model

--- crudWithMvc/server.js
+++ crudWithMvc/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const connect = require("./config/db");
 
+connect;
 dotenv.config();
 const port = process.env.port;
-
-const connect = require("./config/db");
-connect;
 
 app.listen(port, () => {
   console.log(`server is listin in this ${port}`);
