febcb4cdff5aca8e6b3f1f5d6d01037ce008e8a6
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-11-19 12:02:46 +0530

    create db file and connect

--- crudWithMvc/server.js
+++ crudWithMvc/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+const port = process.env.port;
+
+const connect = require("./config/db");
+connect;
 
 app.listen(port, () => {
   console.log(`server is listin in this ${port}`);
