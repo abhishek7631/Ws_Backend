fb727746a6f3c6bd3d751621d49ba7222042f2ef
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-11-21 12:26:38 +0530

    create controllers

--- middleware-02/controllers/user.js
+++ middleware-02/controllers/user.js
@@ -1,1 +1,27 @@
-const User = require("../config/data");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.getAllUser = async (req, res) => {
+  try {
+    res.status(200).json({ message: "This is get route" });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
+
+exports.register = async (req, res) => {
+  try {
+    res.status(200).json({ message: "This is register route" });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    res.status(200).json({ message: "This is login Route" });
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
--- middleware-02/routes/userRoutes.js
+++ middleware-02/routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { getAllUser, register, login } = require("../controllers/user");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "This is all get route" });
-});
+router.get("/", getAllUser);
+
+router.post("/register", register);
+
+router.post("/login", login);
 
 module.exports = router;
