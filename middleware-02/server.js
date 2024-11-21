76615fb6aa69c7816cdf0719c1d3f22618346ffb
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-11-21 12:18:59 +0530

    code modify

--- middleware-02/server.js
+++ middleware-02/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const connect = require("./config/data");
+const userRoute = require("./routes/userRoutes");
 
 connect();
 
 dotenv.config();
 const port = process.env.port;
+
+app.use(express.json());
+app.use("/api", userRoute);
 
 app.listen(port, () => {
   console.log(`server is running at this port ${port}`);
