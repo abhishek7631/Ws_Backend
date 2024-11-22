56fcb0d3fafa7eaa2d0c3f7e9b52ab6f87820528
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-11-22 11:46:51 +0530

    setup project

---
+++ middleware-03/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const port = process.env.port;
+
+app.listen(port, () => {
+  console.log(`server is listen on port ${port}`);
+});
