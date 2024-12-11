ee0644d09e177bf4bf91804d73d3aec43731db9a
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-12-11 12:52:00 +0530

    implement multer

--- multer/backend/server.js
+++ multer/backend/server.js
@@ -1,8 +1,35 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const multer = require("multer");
 const app = express();
 
+mongoose
+  .connect("mongodb://localhost:27017/biodata")
+  .then(() => console.log("database connected"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const biodataSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: String, required: true },
+  photo: String,
+});
+
+const Bio = mongoose.model("Bio", biodataSchema);
+
 const port = 4000;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
+});
+
+const upload = multer({ storage });
+
+app.post("/uploads", upload.single("photo"), (req, res) => {
+  res.status(200).json({ message: "photo has been uploaded successfully" });
+});
 
 app.listen(port, () => {
   console.log(`server is listining on ${port}`);
