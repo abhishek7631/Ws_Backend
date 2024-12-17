0057fe7829bda58f5bd76f0d07f8847ac3734a3b
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-12-17 22:29:56 +0530

    implemet add cart

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,9 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "./store/slices/cart";
 import "./App.css";
 
 function App() {
+  const cart = useSelector((state) => console.log(state.carts));
+
+  const dispatch = useDispatch();
+
   return (
     <>
       <h1>Welcome to Redux</h1>
+
+      <button
+        onClick={() => {
+          dispatch(addToCart({ name: "Abhishek" }));
+        }}
+      >
+        Add
+      </button>
     </>
   );
 }
