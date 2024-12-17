5ae028b65c64d46227fc49cd32d30f95168128c1
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-12-17 22:38:21 +0530

    get data from api

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,8 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "./store/slices/cart";
+import axios from "axios";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const getData = async () => {
+    const response = await axios.get("https://fakestoreapi.com/products");
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const cart = useSelector((state) => console.log(state.carts));
 
   const dispatch = useDispatch();
